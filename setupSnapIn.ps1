c282aa22062e82eb2e3b33451fefc9fd6cb5936a
Spencer Owen <owenspencer@gmail.com>
2016-08-24 11:08:44 -0600

    Support more than just powershell 2
    If a vm does not have powershell 2 present, it will silently fail.

--- setupSnapIn.ps1
+++ setupSnapIn.ps1
@@ -19,7 +19,32 @@ function Install-Snapin()
   foreach ($platform in ("", "64") )
   {
     Write-Host "Registering $assembly on platform '$platform'";
-    $installUtil = "$env:windir\Microsoft.Net\Framework${platform}\v2.0.50727\installUtil.exe";
+    if ( Test-Path "$env:windir\Microsoft.Net\Framework${platform}\v4.0.30319" )
+    {
+      Write-Host 'Powershell v4 present'
+      $installUtil = "$env:windir\Microsoft.Net\Framework${platform}\v4.0.30319\installUtil.exe";
+    }
+    ElseIf ( Test-Path "$env:windir\Microsoft.Net\Framework${platform}\v3.5" )
+    {
+      Write-Host 'Powershell v3.5 present'
+      $installUtil = "$env:windir\Microsoft.Net\Framework${platform}\v3.5\installUtil.exe";
+    }
+    ElseIf ( Test-Path "$env:windir\Microsoft.Net\Framework${platform}\v3.0" )
+    {
+      Write-Host 'Powershell v3.0 present'
+      $installUtil = "$env:windir\Microsoft.Net\Framework${platform}\v3.0\installUtil.exe";
+    }
+    ElseIf ( Test-Path "$env:windir\Microsoft.Net\Framework${platform}\v2.0.507272" )
+    {
+      Write-Host 'Powershell v2 present'
+      $installUtil = "$env:windir\Microsoft.Net\Framework${platform}\v2.0.50727\installUtil.exe";
+    }
+    Else
+    {
+      Write-Host 'Could not find any version of powershell >2 and <=4'
+      Exit 1
+    }
+    
     if ( [System.IO.File]::Exists($installUtil) )
     {
       Set-Alias installUtil $installUtil;
